d6ed85821d96515a24be091b4e9cdf15a725d120
Collin Thompson <collin.thompson@kellogg.northwestern.edu>
2024-05-28 14:10:09 +0000

    5.28 finally got the login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,24 @@
 class SessionsController < ApplicationController
+  
+
   def new
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if @user["password"] == params["password]"]
+        # loin the user
+        flash["notice"] = "You've logged in."
+        redirect_to "/companies"
+      else
+        flash["notice"] = "Unsuccessful login. Password incorrect"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Unsuccessful login. User doesn't exist"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find_by({ "id" => params["id"] })
+  end
+  
+  
   def new
   end
 
@@ -8,7 +14,14 @@ class UsersController < ApplicationController
     @user["last_name"] = params["last_name"]
     @user["email"] = params["email"]
     @user["password"] = params["password"]
+
+    @user["username"] = params["username"]
+
     @user.save
     redirect_to "/"
   end
+
+  def show
+  end
+
 end
