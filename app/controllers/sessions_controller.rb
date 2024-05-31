90f057da8dc0ac67f3c4b3be925836fb34f291ca
Collin Thompson <collin.thompson@kellogg.northwestern.edu>
2024-05-31 20:34:25 +0000

    figured out the user session stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,16 @@ class SessionsController < ApplicationController
     if @user
       if BCrypt::Password.new(@user["password"]) == params["password"]
         # loin the user
+        session["user_id"] = @user["id"]
         flash["notice"] = "You've logged in #{@user["username"]}"
         redirect_to "/places"
       else
         flash["notice"] = "Unsuccessful login. Password incorrect"
-        redirect_to "/sessions/new"
+        redirect_to "/login"
       end
     else
       flash["notice"] = "Unsuccessful login. User doesn't exist"
-      redirect_to "/sessions/new"
+      redirect_to "/login"
     end
   end
 
